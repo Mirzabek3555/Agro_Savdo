33bc6f65ad11b29ac9b9e3f89c43c84bdef9f8cc
Mirzabek3555 <87481054+Mirzabek3555@users.noreply.github.com>
2025-08-02 23:37:49 +0500

    Delete agri directory

--- agri/mainapp/utils.py
+++
@@ -1,7 +0,0 @@
-from gooletrans import Translator
-
-
-def translate_text(text, target_lang='ne'):
-    translator = Translator()
-    translated = translator.translate(text, dest=target_lang)
-    return translated.text
